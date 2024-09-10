f1e2ef67fa1c837417b07537a2412974d7f73460
Edmilson Romão <edmilson08romao@gmail.com>
2024-09-11 00:17:54 +0100

    configurações na minha base de dados

---
+++ server/drizzle.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'drizzle-kit'
+import { env } from './src/env'
+export default defineConfig({
+  schema: "./src/db/schema.ts",
+  out: './.migratios',
+  dialect: 'postgresql',
+  
+  dbCredentials: {
+
+    url: env.DATABASE_URL,
+  }
+})
+
+
+
